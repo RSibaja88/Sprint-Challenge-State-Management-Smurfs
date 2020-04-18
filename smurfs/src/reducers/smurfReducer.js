44491041b0a3d820259489b8f6a06693b8eac4a9
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-04-18 13:38:01 -0400

    set up provider, thunk, reducer, and actions

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,32 @@
+import {GET_SMURF, ADD_SMURF, SMURF_ERROR} from '../actions/smurfAction';
+
+const initalState = {
+    smurfs: [],
+    isFetchingData: false,
+    error: ""
+};
+
+export const smurfReducer = (state = initalState, action) => {
+    switch(action.type) {
+        case GET_SMURF:
+            return {
+                ...state, isFetchingData: true
+            };
+        case ADD_SMURF:
+            return {
+                ...state,
+                smurf: action.payload,
+                isFetchingData: false
+            };
+        case SMURF_ERROR:
+            return {
+                ...state,
+                isFetchingData: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    };
+}
+
+export default smurfReducer;
